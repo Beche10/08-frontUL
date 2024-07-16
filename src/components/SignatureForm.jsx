b0472a153b0d398b0249fbe1a8b46cf21455f96b
Augusto <villevip10@gmail.com>
2024-07-16 16:18:53 -0300

    Adv10

--- src/components/SignatureForm.jsx
+++ src/components/SignatureForm.jsx
@@ -1,15 +1,25 @@
-import React from 'react'
+import React from "react";
+import Popup from "reactjs-popup";
+import SignaturePad from 'react-signature-canvas';
+import 'reactjs-popup/dist/index.css';
+import './sigCanvas.css';
+
 
 export const SignatureForm = () => {
-  
-  
-  
+
+
+
   return (
-    <button
-              type=""
-              className="w-full mt-4 bg-green-500 border border-transparent rounded-md py-2 px-4 inline-flex justify-center items-center text-white hover:bg-green-600 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
-            >
-              Firme Aqui!
-            </button>
-  )
-}
+    
+    <div>
+      <Popup
+        modal
+        trigger={<button> Firme Aquí! </button>}>
+        <SignaturePad canvasProps={{
+          className: "signatureCanvas"
+        }}/>
+      </Popup>
+    </div>
+  
+  );
+};
