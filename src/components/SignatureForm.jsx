f60925d06e3bc2a926f7e82c24ba495b05e46233
Augusto <villevip10@gmail.com>
2024-07-17 01:15:52 -0300

    adv

--- src/components/SignatureForm.jsx
+++ src/components/SignatureForm.jsx
@@ -1,30 +1,38 @@
 import React from "react";
+import { useRef } from "react";
 import Popup from "reactjs-popup";
-import SignaturePad from 'react-signature-canvas';
-import 'reactjs-popup/dist/index.css';
-import './sigCanvas.css';
-
+import SignaturePad from "react-signature-canvas";
+import "reactjs-popup/dist/index.css";
+import "./sigCanvas.css";
 
 export const SignatureForm = () => {
+  const sigCanvas = useRef({});
 
-
+  const limpiar = () => {
+    sigCanvas.current.clear();
+  };
 
   return (
-    
     <div>
-      <Popup modal trigger={<button> Firme Aquí! </button>} closeOnDocumentClick={false}>
-        {close => (
-        <>
-        <SignaturePad canvasProps={{
-          className: "signatureCanvas"
-        }}/>
+      <Popup
+        modal
+        trigger={<button> Firme Aquí! </button>}
+        closeOnDocumentClick={false}
+      >
+        {(close) => (
+          <>
+            <SignaturePad
+              ref={sigCanvas}
+              canvasProps={{
+                className: "signatureCanvas",
+              }}
+            />
 
-        <button onClick={close}>Cerrar</button>
-
-        </>  
-       )}
+            <button onClick={close}>Cerrar</button>
+            <button onClick={limpiar}>Limpiar</button>
+          </>
+        )}
       </Popup>
     </div>
-  
   );
 };
