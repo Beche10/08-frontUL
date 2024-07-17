1b121f0223a6dd57aa4a0d480b3f9df393aa9504
Augusto <villevip10@gmail.com>
2024-07-17 02:00:30 -0300

    adv

--- src/components/SignatureForm.jsx
+++ src/components/SignatureForm.jsx
@@ -9,18 +9,23 @@ export const SignatureForm = () => {
   const sigCanvas = useRef({});
 
   const limpiar = () => {
-    sigCanvas.current.clear()
+    sigCanvas.current.clear();
   };
 
   const guardar = () => {
-      console.log(sigCanvas.current.getTrimmedCanvas().toDataURL("image/png"))
+    console.log(sigCanvas.current.getTrimmedCanvas().toDataURL("image/png"));
   };
 
   return (
     <div>
       <Popup
         modal
-        trigger={<button className="w-full mt-4 bg-green-500 border border-transparent rounded-md py-2 px-4 inline-flex justify-center items-center text-white hover:bg-green-600 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"> Firme Aquí! </button>}
+        trigger={
+          <button className="w-full mt-4 bg-green-500 border border-transparent rounded-md py-2 px-4 inline-flex justify-center items-center text-white hover:bg-green-600 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
+            {" "}
+            Firma{" "}
+          </button>
+        }
         closeOnDocumentClick={false}
       >
         {(close) => (
@@ -32,9 +37,24 @@ export const SignatureForm = () => {
               }}
             />
 
-            <button className="bg-red-500 hover:bg-red-700 text-white font-bold py-1 px-3 rounded" onClick={close}>Cerrar</button>
-            <button className="bg-gray-500 hover:bg-gray-700 text-white font-bold py-1 px-3 rounded" onClick={limpiar}>Limpiar</button>
-            <button className="bg-green-500 hover:bg-green-700 text-white font-bold py-1 px-3 rounded" onClick={guardar}>Guardar</button>
+            <button
+              className="bg-red-500 hover:bg-red-700 text-white font-bold py-1 px-3 rounded"
+              onClick={close}
+            >
+              Cerrar
+            </button>
+            <button
+              className="bg-gray-500 hover:bg-gray-700 text-white font-bold py-1 px-3 rounded"
+              onClick={limpiar}
+            >
+              Limpiar
+            </button>
+            <button
+              className="bg-green-500 hover:bg-green-700 text-white font-bold py-1 px-3 rounded"
+              onClick={guardar}
+            >
+              Guardar
+            </button>
           </>
         )}
       </Popup>
