dcd6c8f7a2b157b097f3b0f5c07c7c1cdb1eeac7
Augusto <villevip10@gmail.com>
2024-07-18 15:34:40 -0300

    adv

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,12 +8,12 @@ export default {
       },
 
       colors: {
-        "green-color": '#27AE60',
-        "gray-color": '#828282',
-        "card-color": '#212121',
-      }
-
-
+        "green-color": "#27AE60",
+        "gray-color": "#828282",
+        "card-color": "#212121",
+        "color-background": "#171717",
+        "menu-color": "#172554",
+      },
     },
   },
   plugins: [],
