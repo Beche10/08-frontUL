2be04bd7cada498c8d6c27d09015f985580110a9
Augusto <villevip10@gmail.com>
2024-07-19 20:37:54 -0300

    adv

--- src/App.jsx
+++ src/App.jsx
@@ -85,9 +85,9 @@ export const App = () => {
             <article className="w-max grid justify-items-center">
 
 
-            <div className="p-4 w-max aspect-square bg-card-color rounded-full">
+            <div className="icons-container">
 
-            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" className="fill-green-color w-10"><path d="M20 4H4a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2V6a2 2 0 0 0-2-2zm0 4.7-8 5.334L4 8.7V6.297l8 5.333 8-5.333V8.7z"></path></svg>
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" className="icons"><path d="M20 4H4a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2V6a2 2 0 0 0-2-2zm0 4.7-8 5.334L4 8.7V6.297l8 5.333 8-5.333V8.7z"></path></svg>
 
             </div>
 
