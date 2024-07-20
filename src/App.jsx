17a2b1d85a0870ae75e6fd9f4f4334267211ea5d
Augusto <villevip10@gmail.com>
2024-07-20 15:48:30 -0300

    adv

--- src/App.jsx
+++ src/App.jsx
@@ -146,10 +146,10 @@ export const App = () => {
 
 <form className="flex flex-wrap justify-between gap-8 px-1 max-w-screen-lg mx-auto">
 
-  <input className="border-b px-2 py-4 flex-grow basis-60 outline-none focus:ring-1 focus:ring-green-color focus:ring-inset transition" type="text" placeholder="Nombre"/>
-  <input className="border-b px-2 py-4 flex-grow basis-60 outline-none focus:ring-1 focus:ring-green-color focus:ring-inset transition" type="email" placeholder="Email"/>
+  <input className="border-b px-2 py-4 flex-grow basis-60 focus-input" type="text" placeholder="Nombre"/>
+  <input className="border-b px-2 py-4 flex-grow basis-60 focus-input" type="email" placeholder="Email"/>
  
-  <textarea className="border px-4 py-6 min-w-full max-w-full w-full min-h-[100px] max-h-60" placeholder="Mensaje"></textarea>
+  <textarea className="border px-4 py-6 min-w-full max-w-full w-full min-h-[100px] max-h-60 focus-input" placeholder="Mensaje"></textarea>
  
  
   <input className="bg-green-color py-5 px-14 mx-auto cursor-pointer" type="submit" value={"Enviar"} />
