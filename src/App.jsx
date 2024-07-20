cdff6817287ca6786d68a96588c13e226708e1d1
Augusto <villevip10@gmail.com>
2024-07-19 21:03:48 -0300

    adv

--- src/App.jsx
+++ src/App.jsx
@@ -96,6 +96,24 @@ export const App = () => {
 
 
             </article>
+
+            <article className="w-max grid justify-items-center">
+
+
+<div className="icons-container">
+
+<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" className="icons"><path d="M20 4H4a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2V6a2 2 0 0 0-2-2zm0 4.7-8 5.334L4 8.7V6.297l8 5.333 8-5.333V8.7z"></path></svg>
+
+</div>
+
+<h3 className="text-2xl mt-4 mb-2">Instagram</h3>
+<p className="font-light">@UnionLibertariaCatamarca</p>
+
+
+</article>
+
+
+
           </div>
         </section>
       </main>
