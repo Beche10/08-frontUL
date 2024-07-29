a6e54397f4e90685adb7366f36173665d6c9b918
Augusto <villevip10@gmail.com>
2024-07-29 13:36:27 -0300

    adv

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -65,10 +65,10 @@ export const Form = () => {
         </nav>
       </header>
 
-      <div className="grid grid-cols-auto grid-rows-auto items-center justify-center gap-8 px-1 max-w-screen-lg mx-auto ">
+      <div className="grid grid-cols-auto grid-rows-auto items-center justify-center gap-8 px-1 max-w-screen-lg mx-auto">
         <form
           onSubmit={onSubmit}
-          className="grid grid-cols-1 grid-rows-auto gap-8 px-3 py-3 max-w-screen-lg mx-auto sm:border-4 rounded-lg"
+          className="grid grid-cols-1 grid-rows-auto gap-8 px-3 py-3 max-w-screen-lg mx-auto rounded-lg"
         >
           <h2 className="bg-indigo-500 px-2 py-4 text-3xl text-center text-white -mb-4 rounded-md col-span-2">
             Preinscripción para afiliados
