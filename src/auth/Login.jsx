c3523feec7de32f0ee6a825b27ad7e672a37c76e
Augusto <villevip10@gmail.com>
2024-08-01 19:57:24 -0300

    adv

--- src/auth/Login.jsx
+++ src/auth/Login.jsx
@@ -1,9 +1,11 @@
-import React from "react";
+import React, { useState } from "react";
 import {
   RiMailLine,
   RiLockLine,
   RiEyeLine,
   RiEyeOffLine,
+  RiMailFill,
+  RiLockFill
 } from "react-icons/ri";
 
 
@@ -21,12 +23,22 @@ export const Login = () => {
         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" className="icons w-4 h-4"><path d="M20.283 10.356h-8.327v3.451h4.792c-.446 2.193-2.313 3.453-4.792 3.453a5.27 5.27 0 0 1-5.279-5.28 5.27 5.27 0 0 1 5.279-5.279c1.259 0 2.397.447 3.29 1.178l2.6-2.599c-1.584-1.381-3.615-2.233-5.89-2.233a8.908 8.908 0 0 0-8.934 8.934 8.907 8.907 0 0 0 8.934 8.934c4.467 0 8.529-3.249 8.529-8.934 0-.528-.081-1.097-.202-1.625z"></path></svg>
             Ingresa con google
           </button>
-          <div>
+          <div className="relative mb-4">
+            <RiMailFill className="absolute top-1/2 -translate-y-1/2 left-2"/>
 
             <input 
             type="email" 
-            className="py-2 px-4 bg-secondary-900 w-full outline-none rounded-lg"
+            className="py-2 pl-8 pr-4 bg-secondary-900 w-full outline-none rounded-lg"
             placeholder="Correo electrónico"
+            />
+          </div>
+          <div className="relative ">
+            <RiLockFill className="absolute top-1/2 -translate-y-1/2 left-2"/>
+
+            <input 
+            type="password" 
+            className="py-2 pl-8 pr-4 bg-secondary-900 w-full outline-none rounded-lg"
+            placeholder="Contraseña"
             />
           </div>
       </form>
