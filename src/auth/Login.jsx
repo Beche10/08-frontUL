001da5f8cb6e1dbb40d9a7ecd8478e0acf6f16ef
Augusto <villevip10@gmail.com>
2024-08-01 17:40:09 -0300

    adv

--- src/auth/Login.jsx
+++ src/auth/Login.jsx
@@ -3,7 +3,7 @@ import React from "react";
 export const Login = () => {
   return (
     <div>
-      <h1>Iniciar Sesión.</h1>
+      <h1 className="text-3xl uppercase font-bold">Iniciar Sesión.</h1>
       <form className="bg-secondary-100">
         <div>
               <button>Ingresa con google</button>
