2087cf8b50490b0a3f6ec97be857b762257e125d
Augusto <villevip10@gmail.com>
2024-08-02 13:33:17 -0300

    adv

--- src/components/Help.jsx
+++ src/components/Help.jsx
@@ -1,7 +1,6 @@
 import React, { useState } from "react";
 import { Link as RouterLink } from "react-router-dom";
 
-
 export const Help = () => {
   const [activeLink, setActiveLink] = useState("");
 
@@ -54,7 +53,62 @@ export const Help = () => {
         </header>
       </div>
 
-      <h1>Help to User</h1>
+      <h1>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam sed laoreet
+        eros, vitae lobortis justo. Cras nunc odio, auctor quis enim sit amet,
+        pretium vestibulum mi. Fusce mauris urna, luctus id ex eget, tristique
+        pulvinar odio. Duis vehicula, sapien id dapibus sagittis, arcu sapien
+        consequat velit, nec semper risus magna ac tellus. Fusce at commodo
+        neque, aliquam faucibus lacus. Etiam vulputate tellus hendrerit vehicula
+        accumsan. Cras purus lorem, porta eget enim quis, aliquam sodales augue.
+        Sed egestas euismod convallis. Phasellus non fermentum eros. Cras
+        pretium commodo metus, eu dictum neque venenatis eu. Ut finibus erat
+        mauris, ut luctus eros condimentum ac. Morbi elementum ac lacus eu
+        aliquam. Nam finibus condimentum quam in accumsan. In hac habitasse
+        platea dictumst. Suspendisse maximus, odio vel faucibus accumsan, magna
+        enim consectetur velit, sed fermentum dui diam nec augue. Duis vel metus
+        sed nulla condimentum placerat eu eget ante. Phasellus sagittis at diam
+        id laoreet. In sed risus neque. Proin sodales rutrum nisi pretium
+        pulvinar. Donec congue rutrum placerat. Vivamus tincidunt, lorem eu
+        sollicitudin ullamcorper, dui velit fermentum metus, suscipit volutpat
+        dolor odio at libero. Maecenas bibendum ante congue nibh volutpat, nec
+        posuere lectus suscipit. Morbi malesuada porttitor risus, vitae
+        vulputate tellus elementum sed. Nam auctor, metus quis suscipit
+        consequat, magna orci dignissim mauris, vel volutpat nisl libero non
+        risus. Nunc cursus ligula accumsan est mattis, eu aliquam est fermentum.
+        Sed laoreet iaculis accumsan. Vestibulum in arcu mauris. Phasellus quam
+        nulla, tempor nec lorem quis, pulvinar faucibus libero. Ut ornare nulla
+        id ligula placerat porttitor. Duis volutpat convallis libero, at
+        hendrerit erat dapibus ut. Maecenas sit amet velit efficitur, maximus
+        nisl id, sodales orci. Nunc commodo lorem et facilisis luctus. In
+        vulputate a est ut viverra. Integer pellentesque venenatis turpis eget
+        blandit. Integer sem velit, mollis quis pretium quis, ornare sit amet
+        nunc. Vivamus sit amet velit neque. Ut non eleifend leo, sed cursus
+        risus. Nam finibus a sapien eget pellentesque. Quisque imperdiet
+        pellentesque dolor, sed pharetra neque sodales eu. Vestibulum dui arcu,
+        imperdiet ac eros eget, molestie ultricies enim. Curabitur interdum nunc
+        eget ante porttitor, eget sollicitudin arcu pretium. Mauris in bibendum
+        nisl, sed facilisis enim. Sed placerat risus massa, ut laoreet sapien
+        venenatis vitae. Vestibulum ante ipsum primis in faucibus orci luctus et
+        ultrices posuere cubilia curae; Nulla congue, lorem vitae aliquet
+        finibus, dolor metus congue justo, sit amet sollicitudin purus metus ut
+        enim. Etiam rhoncus erat congue mattis ultricies. Aliquam iaculis lorem
+        sem, quis eleifend nunc condimentum ac. In vitae dignissim urna. Vivamus
+        scelerisque, est sit amet condimentum dapibus, neque ante iaculis arcu,
+        sit amet porttitor elit elit nec nisl. Etiam suscipit velit at ipsum
+        luctus bibendum. Phasellus dapibus, libero in lacinia imperdiet, mi nisi
+        vulputate risus, sed semper justo elit eget tortor. Donec viverra
+        tincidunt nibh ut finibus. Vivamus cursus sem id diam ultricies lacinia.
+        Sed blandit sodales ornare. Aenean aliquam lorem quis mauris feugiat
+        malesuada. Pellentesque ultricies massa magna, vitae congue nibh aliquet
+        vitae. Vivamus rhoncus, urna sit amet pulvinar aliquet, ligula nisi
+        mattis eros, ac rutrum libero eros a justo. Maecenas id eros neque. Ut
+        sit amet est aliquam, sollicitudin turpis sed, pellentesque mi. Nullam
+        rhoncus ac tellus nec luctus. Integer orci purus, aliquet ac ex vel,
+        tempus dapibus justo. Vivamus volutpat justo in tellus viverra, vel
+        accumsan risus vulputate. Generados 5 párrafos, 541 palabras, 3657 bytes
+        de Lorem Ipsum
+      </h1>
     </div>
   );
 };
