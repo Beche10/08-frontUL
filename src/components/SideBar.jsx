1f4b19f98a204238cb568219976a55164fe60bc2
Augusto <villevip10@gmail.com>
2024-08-02 15:59:31 -0300

    adv

--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -2,10 +2,12 @@ import React from "react";
 
 export const SideBar = () => {
   return (
-    <div className="xl:h-[100vh] overflow-y-scroll fixed xl:static w-full h-full -left-full top-0 bg-secondary-100">
-      <h1>
-        SideBar
-      </h1>
+    <div className="xl:h-[100vh] overflow-y-scroll fixed xl:static w-full h-full -left-full top-0 bg-secondary-100 p-8">
+      <div>
+        <h1>
+            Admin<span className="text-primary">.</span>
+        </h1>
+      </div>
     </div>
   );
 };
