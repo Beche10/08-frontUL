3dc512aac0eec96931b06d6c9f2faca387abaddb
Augusto <villevip10@gmail.com>
2024-08-02 16:14:44 -0300

    adv

--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -1,12 +1,28 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import {
+    RiBarChart2Line,
+    RiEarthLine,
+    RiCustomerService2Line,
+    RiCalendarTodoLine,
+    RiLogoutCircleRLine,
+    RiArrowRightSLine,
+    RiMenu3Line,
+    RiCloseLine,
+  } from "react-icons/ri";
+
 
 export const SideBar = () => {
-  return (
+  
+    return (
     <div className="xl:h-[100vh] overflow-y-scroll fixed xl:static w-full h-full -left-full top-0 bg-secondary-100 p-8">
       <div>
-        <h1>
-            Admin<span className="text-primary">.</span>
+        <h1 className="text-center text-2xl font-bold text-white mb-10">
+            Admin<span className="text-primary text-4xl">.</span>
         </h1>
+        <nav>
+           <Link to="/"><RiBarChart2Line />    </Link> 
+        </nav>
       </div>
     </div>
   );
