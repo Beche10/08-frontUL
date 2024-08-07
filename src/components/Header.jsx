da1a84a77df0682b9ef77494b1c061c1fcf80ecc
Augusto <villevip10@gmail.com>
2024-08-07 11:50:19 -0300

    adv

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -8,22 +8,23 @@ import {
   RiChat3Line,
 } from "react-icons/ri";
 
-
-
-
 export const Header = () => {
   return (
     <header className="font-bold h-[7vh] md:h-[10vh] border-b border-secondary-100 p-8 flex items-center justify-end">
       <nav className="flex items-center gap-x-4">
         <button>
-          <RiNotification3Line/>
+          <RiNotification3Line />
         </button>
         <button>
-        <img src="/logoHD.jpg" alt="Logo" className="w-6 h-6 object-cover rounded-full" />
+          <img
+            src="/logoHD.jpg"
+            alt="Logo"
+            className="w-6 h-6 object-cover rounded-full"
+          />
+          <span>Usuario</span>
+          <RiArrowDownSLine/>
         </button>
       </nav>
-
-     
     </header>
   );
 };
