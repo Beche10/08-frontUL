53ca57bf5088d77660b8a2e193f6b578cf8088f4
Augusto <villevip10@gmail.com>
2024-08-07 15:24:38 -0300

    adv

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -11,27 +11,39 @@ import { Menu, MenuItem, MenuButton } from "@szhsin/react-menu";
 import "@szhsin/react-menu/dist/index.css";
 import "@szhsin/react-menu/dist/transitions/slide.css";
 
-
-
-
-
 export const Header = () => {
   return (
     <header className="font-bold h-[7vh] md:h-[10vh] border-b border-secondary-100 p-8 flex items-center justify-end">
       <nav className="flex items-center gap-x-2">
         <button className="relative hover:bg-secondary-100 p-2 rounded-lg transition-colors">
           <RiNotification3Line />
-          <span className="absolute -top-0.5 right-0 bg-primary py-0.5 px-[5px] text-white rounded-full text-[9px] box-content font-bold">2</span>
+          <span className="absolute -top-0.5 right-0 bg-primary py-0.5 px-[5px] text-white rounded-full text-[9px] box-content font-bold">
+            2
+          </span>
         </button>
-        <button className="flex items-center gap-x-2 hover:bg-secondary-100 py-2 px-4 rounded-lg transition-colors">
-          <img
-            src="/logoHD.jpg"
-            alt="Logo"
-            className="w-6 h-6 object-cover rounded-full"
-          />
-          <span>Usuario</span>
-          <RiArrowDownSLine/>
-        </button>
+        <Menu
+          menuButton={
+            <MenuButton>
+              {" "}
+              <img
+                src="/logoHD.jpg"
+                alt="Logo"
+                className="w-6 h-6 object-cover rounded-full"
+              />
+              <span>Usuario</span>
+              <RiArrowDownSLine />
+            </MenuButton>
+          }
+          transition
+          className="flex items-center gap-x-2 hover:bg-secondary-100 py-2 px-4 rounded-lg transition-colors"
+        >
+
+              <MenuItem>New File</MenuItem>
+              <MenuItem>Save</MenuItem>
+              <MenuItem>Close Window</MenuItem>
+
+
+        </Menu>
       </nav>
     </header>
   );
