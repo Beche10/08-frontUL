199e598ca8351234772913d0f4050b344de9f921
Augusto <villevip10@gmail.com>
2024-08-06 23:18:39 -0300

    adv

--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -13,100 +13,110 @@ import {
 } from "react-icons/ri";
 
 export const SideBar = () => {
+  const [showMenu, setShowMenu] = useState(false);
   const [showSubMenu, setShowSubMenu] = useState(false);
 
   return (
-<>
-<div className="xl:h-[100vh] overflow-y-scroll fixed xl:static w-full h-full -left-full top-0 bg-secondary-100 p-4 flex flex-col justify-between">
-      <div>
-        <h1 className="text-center text-2xl font-bold text-white mb-10">
-          Admin<span className="text-primary text-4xl">.</span>
-        </h1>
-        <ul>
-          <li>
-            <Link
-              to="/"
-              className="flex items-center gap-4 py-2 px-4 rounded-lg hover:bg-secondary-900 transition-colors"
-            >
-              <RiBarChart2Line className="text-primary" /> Analíticas
-            </Link>
-          </li>
+    <>
+      <div
+        className={`xl:h-[100vh] overflow-y-scroll fixed xl:static w-[65%] md:w-[40%] lg:w-[30%] xl:w-auto h-full top-0 bg-secondary-100 p-4 flex flex-col justify-between z-50 ${
+          showMenu ? "left-0" : "-left-full"
+        } transition-all`}
+      >
+        <div>
+          <h1 className="text-center text-2xl font-bold text-white mb-10">
+            Admin<span className="text-primary text-4xl">.</span>
+          </h1>
+          <ul>
+            <li>
+              <Link
+                to="/"
+                className="flex items-center gap-4 py-2 px-4 rounded-lg hover:bg-secondary-900 transition-colors"
+              >
+                <RiBarChart2Line className="text-primary" /> Analytics
+              </Link>
+            </li>
 
-          <li>
-            <button
-              to="/"
-              onClick={() => setShowSubMenu(!showSubMenu)}
-              className="w-full flex items-center gap-4 py-2 px-4 rounded-lg hover:bg-secondary-900 transition-colors"
-            >
-              <span className="flex items-center gap-4">
-                <RiEarthLine className="text-primary" /> Social Media
-              </span>
-              <RiArrowRightSLine className={`mt-1 ${showSubMenu && "rotate-90"} transition-all`} />
-            </button>
-            <ul className={`my-2 ${!showSubMenu && "hidden"} `}>
-              <li>
-                <Link
-                  to="/"
-                  className="py-2 px-4 border-l border-gray-500 ml-6 block relative before:w-3 before:h-3 before:absolute before:bg-primary before:rounded-full before:-left-[6.5px] before:top-1/2 before:-translate-y-1/2 before:border-4 before:border-secondary-100 hover:text-primary transition-colors"
-                >
-                  Social Post
-                </Link>
-              </li>
-              <li>
-                <Link
-                  to="/"
-                  className="py-2 px-4 border-l border-gray-500 ml-6 block relative before:w-3 before:h-3 before:absolute before:bg-gray-500 before:rounded-full before:-left-[6.5px] before:top-1/2 before:-translate-y-1/2 before:border-4 before:border-secondary-100 hover:text-primary transition-colors"
-                >
-                  Social Statistic
-                </Link>
-              </li>
-              <li>
-                <Link
-                  to=""
-                  className="py-2 px-4 border-l border-gray-500 ml-6 block relative before:w-3 before:h-3 before:absolute before:bg-gray-500 before:rounded-full before:-left-[6.5px] before:top-1/2 before:-translate-y-1/2 before:border-4 before:border-secondary-100 hover:text-primary transition-colors"
-                >
-                  Followers
-                </Link>
-              </li>
-            </ul>
-          </li>
+            <li>
+              <button
+                to="/"
+                onClick={() => setShowSubMenu(!showSubMenu)}
+                className="w-full flex items-center gap-4 py-2 px-4 rounded-lg hover:bg-secondary-900 transition-colors"
+              >
+                <span className="flex items-center gap-4">
+                  <RiEarthLine className="text-primary" /> Social Media
+                </span>
+                <RiArrowRightSLine
+                  className={`mt-1 ${
+                    showSubMenu && "rotate-90"
+                  } transition-all`}
+                />
+              </button>
+              <ul className={`my-2 ${!showSubMenu && "hidden"} `}>
+                <li>
+                  <Link
+                    to="/"
+                    className="py-2 px-4 border-l border-gray-500 ml-6 block relative before:w-3 before:h-3 before:absolute before:bg-primary before:rounded-full before:-left-[6.5px] before:top-1/2 before:-translate-y-1/2 before:border-4 before:border-secondary-100 hover:text-primary transition-colors"
+                  >
+                    Social Post
+                  </Link>
+                </li>
+                <li>
+                  <Link
+                    to="/"
+                    className="py-2 px-4 border-l border-gray-500 ml-6 block relative before:w-3 before:h-3 before:absolute before:bg-gray-500 before:rounded-full before:-left-[6.5px] before:top-1/2 before:-translate-y-1/2 before:border-4 before:border-secondary-100 hover:text-primary transition-colors"
+                  >
+                    Social Statistic
+                  </Link>
+                </li>
+                <li>
+                  <Link
+                    to=""
+                    className="py-2 px-4 border-l border-gray-500 ml-6 block relative before:w-3 before:h-3 before:absolute before:bg-gray-500 before:rounded-full before:-left-[6.5px] before:top-1/2 before:-translate-y-1/2 before:border-4 before:border-secondary-100 hover:text-primary transition-colors"
+                  >
+                    Followers
+                  </Link>
+                </li>
+              </ul>
+            </li>
 
-          <li>
-            <Link
-              to="/"
-              className="flex items-center gap-4 py-2 px-4 rounded-lg hover:bg-secondary-900 transition-colors"
-            >
-              <RiMessage2Line className="text-primary" />
-              Messanges
-            </Link>
-          </li>
-          <li>
-            <Link
-              to="/"
-              className="flex items-center gap-4 py-2 px-4 rounded-lg hover:bg-secondary-900 transition-colors"
-            >
-              <RiCalendarTodoLine className="text-primary" />
-              Calendar
-            </Link>
-          </li>
-        </ul>
+            <li>
+              <Link
+                to="/"
+                className="flex items-center gap-4 py-2 px-4 rounded-lg hover:bg-secondary-900 transition-colors"
+              >
+                <RiMessage2Line className="text-primary" />
+                Messanges
+              </Link>
+            </li>
+            <li>
+              <Link
+                to="/"
+                className="flex items-center gap-4 py-2 px-4 rounded-lg hover:bg-secondary-900 transition-colors"
+              >
+                <RiCalendarTodoLine className="text-primary" />
+                Calendar
+              </Link>
+            </li>
+          </ul>
+        </div>
+        <nav>
+          <Link
+            to="/"
+            className="flex items-center gap-4 py-2 px-4 rounded-lg hover:bg-secondary-900 transition-colors"
+          >
+            <RiLogoutCircleRLine className="text-primary" />
+            Cerrar sesión
+          </Link>
+        </nav>
       </div>
-      <nav>
-        <Link
-          to="/"
-          className="flex items-center gap-4 py-2 px-4 rounded-lg hover:bg-secondary-900 transition-colors"
-        >
-          <RiLogoutCircleRLine className="text-primary" />
-          Cerrar sesión
-        </Link>
-      </nav>
-</div>
-<button className="fixed bottom-4 right-4 bg-primary text-black p-3 rounded-lg cursor-pointer">
-<RiMenu3Line/>
-
-</button>
-</>
-
-
+      <button
+        onClick={() => setShowMenu(!showMenu)}
+        className="fixed bottom-4 right-4 bg-green-color text-black p-3 rounded-lg cursor-pointer z-50 xl:hidden"
+      >
+        {showMenu ?  <RiCloseLine /> :  <RiMenu3Line /> } 
+       
+      </button>
+    </>
   );
 };
