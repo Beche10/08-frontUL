1c480665dfeadd5730983017ff7b7f633236c224
Augusto <villevip10@gmail.com>
2024-08-14 20:16:14 -0300

    adv

--- src/admin/Profile.jsx
+++ src/admin/Profile.jsx
@@ -1,11 +1,22 @@
-import React from 'react'
+import React from "react";
 
 export const Profile = () => {
-  
-  
-    return (
-    <h1>
-        Profile
-    </h1>
-  )
+  return (
+    <div className="bg-secondary-100 p-8 rounded-xl">
+      <h1 className="text-xl text-gray-100">Profile</h1>
+      <hr className="my-8 border-gray-500" />
+      <form>
+        <div className="flex items-center">
+          
+          <div className="bg-red-100 w-1/4">
+            <p>Avatar</p>
+          </div>
+          <div className="flex-1">
+            <label> avatar </label>
+          </div>
+        
+        </div>
+      </form>
+    </div>
+  );
 };
