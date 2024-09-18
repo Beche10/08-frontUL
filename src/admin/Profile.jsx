e003d4bc4beda8abb1cba6b8f625aa28b68b22a0
Augusto <villevip10@gmail.com>
2024-09-18 11:17:35 -0300

    adv

--- src/admin/Profile.jsx
+++ src/admin/Profile.jsx
@@ -131,13 +131,13 @@ export const Profile = () => {
             </div>
           </div>
           <hr className="my-8 border-gray-500/30 border-dashed" />
-          <div className="flex items-center justify-between">
+          <div className="flex flex-col md:flex-row md:items-center gap-y-4 justify-between">
             <div>
               <h5 className="text-gray-100 text-xl mb-1">Contraseña</h5>
               <p className="text-gray-500 text-sm">******************</p>
             </div>
             <div>
-              <button className="w-full md:auto bg-secondary-900/50 py-3 px-4 rounded-lg hover:bg-secondary-900 transition-colors hover:text-gray-100">
+              <button className="w-full md:w-auto bg-secondary-900/50 py-3 px-4 rounded-lg hover:bg-secondary-900 hover:text-gray-100 transition-colors">
                 Cambiar contraseña
               </button>
             </div>
