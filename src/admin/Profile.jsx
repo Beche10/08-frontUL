6e4e6b31fbd9bf10dc9672a5c6cb60c0a28d625c
Augusto <villevip10@gmail.com>
2024-09-18 19:55:56 -0300

    adv

--- src/admin/Profile.jsx
+++ src/admin/Profile.jsx
@@ -187,9 +187,24 @@ export const Profile = () => {
 
         <form className="mb-8">
           <div className="flex flex-col md:flex-row md:items-center gap-y-4 justify-between">
-            <div>
-              <h5 className="text-gray-100 text-xl mb-1">Correo electrónico</h5>
-              <p className="text-gray-500 text-sm">unionlibertaria@gmail.com</p>
+            <div className="flex items-center gap-4">
+              <img
+                className="w-8 h-8 object-cover rounded-lg"
+                src="https://res.cloudinary.com/dmbgkqeny/image/upload/v1726699708/Tiktok_logo_icfxvg.jpg"
+                alt=""
+              />
+              {/*<svg
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 24 24"
+                className="w-8 h-8 object-cover"
+              >
+                <path d="M19.59 6.69a4.83 4.83 0 0 1-3.77-4.25V2h-3.45v13.67a2.89 2.89 0 0 1-5.2 1.74 2.89 2.89 0 0 1 2.31-4.64 2.93 2.93 0 0 1 .88.13V9.4a6.84 6.84 0 0 0-1-.05A6.33 6.33 0 0 0 5 20.1a6.34 6.34 0 0 0 10.86-4.43v-7a8.16 8.16 0 0 0 4.77 1.52v-3.4a4.85 4.85 0 0 1-1-.1z"></path>
+              </svg>
+              */}
+              <div className="flex flex-col gap-y-1">
+                <h5 className="text-gray-100">TikTok</h5>
+                <p className="text-gray-500 text-sm">Plan properly your workflow</p>
+              </div>
             </div>
             <div>
               <button className="w-full md:w-auto bg-secondary-900/50 py-3 px-4 rounded-lg hover:bg-secondary-900 hover:text-gray-100 transition-colors">
