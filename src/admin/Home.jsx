0038c8b23745ebc3700e4c869c8418bd3a15bb72
Augusto <villevip10@gmail.com>
2024-09-19 18:15:38 -0300

    adv

--- src/admin/Home.jsx
+++ src/admin/Home.jsx
@@ -25,23 +25,18 @@ export const Home = () => {
   };
 
   return (
-    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-8">
-      <div>
+    <div>
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-8">
         {/* CARD */}
         <div>
           <GiLion className="text-4xl bg-pink-500/10 text-pink-500 p-2 box-content rounded-xl" />
-
           <Menu
             menuButton={
               <MenuButton
                 onClick={handleMenuToggle}
                 className="flex items-center gap-x-2 p-2 hover:bg-secondary-100 rounded-lg transition-colors duration-500"
               >
-                <RiMore2Fill
-                  className={`transition all duration-300 ${
-                    showSubMenu ? "rotate-120" : "-rotate-90"
-                  } `}
-                />
+                <RiMore2Fill className="" />
               </MenuButton>
             }
             transition
