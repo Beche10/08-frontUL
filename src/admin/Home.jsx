5c31f710a04d4846eb38c91160c29b51d5611afa
Augusto <villevip10@gmail.com>
2024-09-19 18:58:26 -0300

    adv

--- src/admin/Home.jsx
+++ src/admin/Home.jsx
@@ -49,7 +49,7 @@ export const Home = () => {
               <MenuItem className="p-0 hover:bg-transparent">
                 <Link
                   to="/admin/perfil"
-                  className="rounded-lg transition-colors text-gray-300 flex items-center gap-x-4 py-2 px-6 flex-1"
+                  className="rounded-lg transition-colors text-gray-300 hover:bg-secondary-900 flex items-center gap-x-4 py-2 px-6 flex-1"
                 >
                   Ver todos
                 </Link>
