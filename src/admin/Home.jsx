7c31feb58c6c9526555210a45fbd7aa3e581b9d1
Augusto <villevip10@gmail.com>
2024-09-19 18:00:30 -0300

    adv

--- src/admin/Home.jsx
+++ src/admin/Home.jsx
@@ -30,87 +30,7 @@ export const Home = () => {
         {/* CARD */}
         <div>
           <GiLion className="text-4xl bg-pink-500/10 text-pink-500 p-2 box-content rounded-xl" />
-          <Menu
-            menuButton={
-              <MenuButton className="relative p-2 rounded-lg hover:bg-secondary-100 transition-colors">
-                <RiNotification3Line />
-                <span className="absolute -top-0.5 right-0 bg-primary py-0.5 px-[5px] box-content text-white rounded-full text-[8px] font-bold">
-                  2
-                </span>
-              </MenuButton>
-            }
-            transition
-            theming="dark"
-            arrow={display === "arrow"}
-          >
-            <h1 className="text-gray-300 text-center font-medium">
-              Notificaciones (2)
-            </h1>
-            <hr className="my-6 border-gray-500" />
-            <MenuItem className="p-0 hover:bg-transparent">
-              <Link
-                to="/"
-                className="text-gray-300 flex flex-1 items-center gap-4 py-2 px-4 rounded-lg"
-              >
-                <img
-                  src="logoHD.jpg"
-                  className="w-8 h-8 object-cover rounded-full"
-                />
-                <div className="text-sm flex flex-col">
-                  <div className="flex items-center justify-between gap-4">
-                    <span>Usuario</span>{" "}
-                    <span className="text-[8px]">21/10/2022</span>
-                  </div>
-                  <p className="text-gray-500 text-xs">
-                    Lorem ipsum dolor sit amet...
-                  </p>
-                </div>
-              </Link>
-            </MenuItem>
-            <MenuItem className="p-0 hover:bg-transparent">
-              <Link
-                to="/"
-                className="text-gray-300 flex flex-1 items-center gap-4 py-2 px-4 transition-colors rounded-lg"
-              >
-                <RiThumbUpLine className="p-2 bg-blue-200 text-blue-700 box-content rounded-full" />
-                <div className="text-sm flex flex-col">
-                  <div className="flex items-center justify-between gap-4">
-                    <span>Nuevo like</span>{" "}
-                    <span className="text-[8px]">21/10/2022</span>
-                  </div>
-                  <p className="text-gray-500 text-xs">
-                    A Jorge Trejo le gusta tu pub...
-                  </p>
-                </div>
-              </Link>
-            </MenuItem>
-            <MenuItem className="p-0 hover:bg-transparent">
-              <Link
-                to="/"
-                className="text-gray-300 flex flex-1 items-center gap-4 py-2 px-4 transition-colors rounded-lg"
-              >
-                <RiChat3Line className="p-2 bg-yellow-200 text-yellow-700 box-content rounded-full" />
-                <div className="text-sm flex flex-col">
-                  <div className="flex items-center justify-between gap-4">
-                    <span>Nuevo comentario</span>{" "}
-                    <span className="text-[8px]">21/10/2022</span>
-                  </div>
-                  <p className="text-gray-500 text-xs">
-                    Jorge Trejo ha comentado tu...
-                  </p>
-                </div>
-              </Link>
-            </MenuItem>
-            <hr className="my-6 border-gray-500" />
-            <MenuItem className="p-0 hover:bg-transparent flex justify-center cursor-default">
-              <Link
-                to="/"
-                className="text-gray-400 text-sm hover:text-white transition-colors"
-              >
-                Todas las notificaciones
-              </Link>
-            </MenuItem>
-          </Menu>
+
           <Menu
             menuButton={
               <MenuButton
@@ -144,22 +64,6 @@ export const Home = () => {
               </Link>
             </MenuItem>
             <hr className="my-4 border-gray-500" />
-            <MenuItem className="p-0 hover:bg-transparent">
-              <Link
-                to="/configuracion"
-                className="rounded-lg transition-colors text-gray-300 flex items-center gap-x-4 py-2 px-6 flex-1"
-              >
-                <RiSettings3Line /> Configuración
-              </Link>
-            </MenuItem>
-            <MenuItem className="p-0 hover:bg-transparent">
-              <Link
-                to="/cerrar-sesion"
-                className="rounded-lg transition-colors text-gray-300 flex items-center gap-x-4 py-2 px-6 flex-1"
-              >
-                <RiLogoutCircleRLine /> Cerrar sesión
-              </Link>
-            </MenuItem>
           </Menu>
         </div>
       </div>
