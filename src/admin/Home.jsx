575e0eb6e099617f83f8054e0588d76da9ea9397
Augusto <villevip10@gmail.com>
2024-09-19 19:26:20 -0300

    adv

--- src/admin/Home.jsx
+++ src/admin/Home.jsx
@@ -29,40 +29,40 @@ export const Home = () => {
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-8">
         {/* CARD */}
         <div className="bg-secondary-100 p-8 rounded-xl">
-        <div className="flex items-center justify-between">
-          <div>
-            <GiLion className="text-4xl bg-pink-500/10 text-pink-500 p-2 box-content rounded-xl" />
+          <div className="flex items-center justify-between mb-4">
+            <div>
+              <GiLion className="text-4xl bg-pink-500/10 text-pink-500 p-2 box-content rounded-xl" />
+            </div>
+            <div>
+              <Menu
+                menuButton={
+                  <MenuButton
+                    onClick={handleMenuToggle}
+                    className="flex items-center gap-x-2 p-2 hover:bg-secondary-900 rounded-lg transition-colors duration-500"
+                  >
+                    <RiMore2Fill className="" />
+                  </MenuButton>
+                }
+                transition
+                theming="dark"
+                arrow={display === "arrow"}
+              >
+                <MenuItem className="p-0 hover:bg-transparent">
+                  <Link
+                    to="/admin/perfil"
+                    className="rounded-lg transition-colors text-gray-300 flex items-center p-1 flex-1"
+                  >
+                    Ver todos
+                  </Link>
+                </MenuItem>
+              </Menu>
+            </div>
+            {/* TOTAL DE AFILIADOS */}
+            <div>
+              <h1 className="text-4xl text-white font-bold">1400</h1>
+              <p>Afiliados totales</p>
+            </div>
           </div>
-          <div>
-            <Menu
-              menuButton={
-                <MenuButton
-                  onClick={handleMenuToggle}
-                  className="flex items-center gap-x-2 p-2 hover:bg-secondary-900 rounded-lg transition-colors duration-500"
-                >
-                  <RiMore2Fill className="" />
-                </MenuButton>
-              }
-              transition
-              theming="dark"
-              arrow={display === "arrow"}
-            >
-              <MenuItem className="p-0 hover:bg-transparent">
-                <Link
-                  to="/admin/perfil"
-                  className="rounded-lg transition-colors text-gray-300 flex items-center p-1 flex-1"
-                >
-                  Ver todos
-                </Link>
-              </MenuItem>
-            </Menu>
-          </div>
-
-          {/* TOTAL DE AFILIADOS */}
-          <div>
-            <h1>1400</h1>
-          </div>
-        </div>
         </div>
       </div>
     </div>
