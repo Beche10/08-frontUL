b913113056680a2a11a4b606da821f325823ad1d
Augusto <villevip10@gmail.com>
2024-09-19 18:37:38 -0300

    adv

--- src/admin/Home.jsx
+++ src/admin/Home.jsx
@@ -32,37 +32,38 @@ export const Home = () => {
           <div>
             <GiLion className="text-4xl bg-pink-500/10 text-pink-500 p-2 box-content rounded-xl" />
           </div>
-
-          <Menu
-            menuButton={
-              <MenuButton
-                onClick={handleMenuToggle}
-                className="flex items-center gap-x-2 p-2 hover:bg-secondary-100 rounded-lg transition-colors duration-500"
-              >
-                <RiMore2Fill className="" />
-              </MenuButton>
-            }
-            transition
-            theming="dark"
-            arrow={display === "arrow"}
-          >
-            <MenuItem className="p-0 hover:bg-transparent">
-              <Link
-                to="/admin/perfil"
-                className="rounded-lg transition-colors text-gray-300 flex items-center gap-x-4 py-2 px-6 flex-1"
-              >
-                <img
-                  src="/logoHD.jpg"
-                  className="w-8 h-8 object-cover rounded-full"
-                />
-                <div className="flex flex-col text-sm">
-                  <span className="text-sm">Usuario</span>
-                  <span className="text-xs text-gray-500">UL@gmail.com</span>
-                </div>
-              </Link>
-            </MenuItem>
-            <hr className="my-4 border-gray-500" />
-          </Menu>
+          <div>
+            <Menu
+              menuButton={
+                <MenuButton
+                  onClick={handleMenuToggle}
+                  className="flex items-center gap-x-2 p-2 hover:bg-secondary-100 rounded-lg transition-colors duration-500"
+                >
+                  <RiMore2Fill className="" />
+                </MenuButton>
+              }
+              transition
+              theming="dark"
+              arrow={display === "arrow"}
+            >
+              <MenuItem className="p-0 hover:bg-transparent">
+                <Link
+                  to="/admin/perfil"
+                  className="rounded-lg transition-colors text-gray-300 flex items-center gap-x-4 py-2 px-6 flex-1"
+                >
+                  <img
+                    src="/logoHD.jpg"
+                    className="w-8 h-8 object-cover rounded-full"
+                  />
+                  <div className="flex flex-col text-sm">
+                    <span className="text-sm">Usuario</span>
+                    <span className="text-xs text-gray-500">UL@gmail.com</span>
+                  </div>
+                </Link>
+              </MenuItem>
+              <hr className="my-4 border-gray-500" />
+            </Menu>
+          </div>
         </div>
       </div>
     </div>
