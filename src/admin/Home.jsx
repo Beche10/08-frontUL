2ad7225115cce2413250d50446b8f90337d09500
Augusto <villevip10@gmail.com>
2024-09-20 19:39:36 -0300

    adv

--- src/admin/Home.jsx
+++ src/admin/Home.jsx
@@ -1,17 +1,43 @@
 import React from "react";
 import { Card } from "../components/Card";
 import { GiLion } from "react-icons/gi";
+import { AiFillTikTok } from "react-icons/ai";
+import { FaInstagram } from "react-icons/fa6";
 
 export const Home = () => {
   return (
-    <div>
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-8">
+    <div className="grid grid-cols-auto md:grid-cols-auto lg:grid-cols-3 xl:grid-cols-4 gap-8">
+      <div>
         {/* CARD */}
         <Card
           icon={GiLion}
           mainStat="145,000"
           description="Afiliados totales"
           growth={5.2}
+          isGrowthPositive={true}
+          link="/admin/perfil"
+        />
+      </div>
+
+      <div>
+        {/* CARD */}
+        <Card
+          icon={AiFillTikTok}
+          mainStat="27,125"
+          description="Seguidores totales"
+          growth={1.2}
+          isGrowthPositive={false}
+          link="/admin/perfil"
+        />
+      </div>
+
+      <div>
+        {/* CARD */}
+        <Card
+          icon={FaInstagram}
+          mainStat="11,332"
+          description="Seguidores totales"
+          growth={3.7}
           isGrowthPositive={true}
           link="/admin/perfil"
         />
