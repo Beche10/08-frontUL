bf211d7cbaf382f4bcc454061aaeedd7735a17ea
Augusto <villevip10@gmail.com>
2024-09-23 18:13:33 -0300

    adv

--- src/components/messages.jsx
+++ src/components/messages.jsx
@@ -1,32 +1,19 @@
 import React from "react";
 
-export const Messages = (props) => { 
+export const Messages = (props) => {
   const {
     nombre, // Nombre de usuario
-    email, // Email del usuario
+    correo, // Email del usuario
     mensaje, // Cuerpo de la consulta
   } = props;
+
+
   
-const mensajes = [
-    {
-      id: 1,
-      nombre: "Juan Pérez",
-      email: "juan.perez@example.com",
-      mensaje: "Me gustaría saber más sobre el proceso de afiliación.",
-    },
-    {
-      id: 2,
-      nombre: "Ana López",
-      email: "ana.lopez@example.com",
-      mensaje: "¿Hay alguna reunión programada próximamente?",
-    },
-    {
-      id: 3,
-      nombre: "Carlos García",
-      email: "carlos.garcia@example.com",
-      mensaje: "Estoy interesado en colaborar como voluntario.",
-    },
-  ];
+
+
+
+
+
 
   return (
     <div>
@@ -43,25 +30,24 @@ const mensajes = [
         </div>
 
         {/* Listado de mensajes */}
-        {mensajes.map((mensaje) => (
-          <div
-            key={mensaje.id}
-            className="grid grid-cols-1 md:grid-cols-3 gap-4 items-start mb-4 bg-secondary-900 p-4 rounded-xl"
-          >
-            <div>
-              <h5 className="md:hidden text-white font-bold">Nombre</h5>
-              <p>{mensaje.nombre}</p>
-            </div>
-            <div>
-              <h5 className="md:hidden text-white font-bold">Email</h5>
-              <p>{mensaje.email}</p>
-            </div>
-            <div>
-              <h5 className="md:hidden text-white font-bold">Mensaje</h5>
-              <p>{mensaje.mensaje}</p>
-            </div>
+
+        <div
+          key={mensaje.id}
+          className="grid grid-cols-1 md:grid-cols-3 gap-4 items-start mb-4 bg-secondary-900 p-4 rounded-xl"
+        >
+          <div>
+            <h5 className="md:hidden text-white font-bold">Nombre</h5>
+            <p>{nombre}</p>
           </div>
-        ))}
+          <div>
+            <h5 className="md:hidden text-white font-bold">Email</h5>
+            <p>{correo}</p>
+          </div>
+          <div>
+            <h5 className="md:hidden text-white font-bold">Mensaje</h5>
+            <p>{mensaje}</p>
+          </div>
+        </div>
       </div>
     </div>
   );
