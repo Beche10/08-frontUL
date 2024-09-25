9450f0bb4439ed776280d9487d2696473504fbe5
Augusto <villevip10@gmail.com>
2024-09-25 19:22:59 -0300

    adv

--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -92,7 +92,7 @@ export const SideBar = () => {
             </li>
             <li>
               <Link
-                to="/"
+                to="/admin/afiliados"
                 className="flex items-center gap-4 py-2 px-4 rounded-lg hover:bg-secondary-900 transition-colors"
               >
                 <IoPersonSharp className="text-primary" />
