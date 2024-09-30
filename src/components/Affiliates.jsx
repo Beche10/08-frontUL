7e0eb105fc31b44be60f48b0136188e8043141f9
Augusto <villevip10@gmail.com>
2024-09-30 19:11:47 -0300

    adv

--- src/components/Affiliates.jsx
+++ src/components/Affiliates.jsx
@@ -46,18 +46,19 @@ export const Affiliates = () => {
       </div>
       <div className="bg-secondary-100 p-8 rounded-xl">
         {/* Encabezado */}
-        <div className="hidden md:grid grid-cols-4 gap-4 mb-2 p-2">
+        <div className="hidden md:grid grid-cols-5 gap-4 mb-2 p-2">
           <h5>Fecha</h5>
           <h5>Nombre</h5>
           <h5>Ciudad</h5>
           <h5>Contacto</h5>
+          <h5>Acción</h5>
         </div>
 
         {/* Listado de mensajes */}
         {afiliados.map((afiliados) => (
           <div
             key={afiliados._id} // Suponemos que el ID es _id, ajusta según tu modelo
-            className="grid grid-cols-1 md:grid-cols-4 gap-2 items-start mb-4 bg-secondary-900 p-2 rounded-xl"
+            className="grid grid-cols-1 md:grid-cols-5 gap-2 items-start mb-4 bg-secondary-900 p-2 rounded-xl"
           >
             <div>
               <h5 className="md:hidden text-white font-bold">Fecha</h5>
@@ -79,6 +80,10 @@ export const Affiliates = () => {
             </div>
             <div>
               <h5 className="md:hidden text-white font-bold">Contacto</h5>
+              <p>{afiliados.celular}</p>
+            </div>
+            <div>
+              <h5 className="md:hidden text-white font-bold">Acción</h5>
               <p>{afiliados.celular}</p>
             </div>
           </div>
