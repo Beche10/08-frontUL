0266305a44da7d75cd60e377a019a948be4155dc
Augusto <villevip10@gmail.com>
2024-10-04 01:11:23 -0300

    adv

--- src/components/Affiliates.jsx
+++ src/components/Affiliates.jsx
@@ -4,7 +4,6 @@ import DropDownActions from "../utils/DropDownActions";
 import { IoMdArrowDropleft, IoMdArrowDropright } from "react-icons/io";
 import { FaSearch } from "react-icons/fa";
 
-
 export const Affiliates = () => {
   const [afiliados, setAfiliados] = useState([]);
   const [loading, setLoading] = useState(true);
@@ -73,14 +72,13 @@ export const Affiliates = () => {
 
       <div className="w-full md:w-52 relative mb-3 flex items-center rounded-full overflow-hidden bg-secondary-100">
         <div>
-          <FaSearch className="px-2 text-green-color rounded-full md:text-3xl text-4xl" />{" "}
-          {/* Tamaño más grande en móviles */}
+          <FaSearch className="px-2 text-green-color rounded-full  md:text-3xl text-4xl" />{" "}
         </div>
         <div className="w-full">
           <input
             type="text"
             placeholder="Buscar"
-            className="bg-secondary-100 text-gray-200 rounded-full focus-input px-1.5 w-full sm:text-lg sm:px-4" // Texto más grande y padding mayor en móviles
+            className="bg-secondary-100 text-gray-200 rounded-full focus-input px-1.5 w-full sm:text-lg sm:px-4"
             value={buscarAfiliado}
             onChange={(e) => setBuscarAfiliado(e.target.value)}
           />
