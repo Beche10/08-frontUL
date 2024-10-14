21a1d849eef5f552899e4e726c1fd77fa0249337
Augusto <villevip10@gmail.com>
2024-10-14 18:56:12 -0300

    adv

--- src/utils/DownloadAffiliates.jsx
+++ src/utils/DownloadAffiliates.jsx
@@ -14,7 +14,7 @@ export const DownloadAffiliates = () => {
       const url = window.URL.createObjectURL(new Blob([response.data]));
       const link = document.createElement("a");
       link.href = url;
-      link.setAttribute("download", "afiliados.xlsx"); // Nombre del archivo
+      link.setAttribute("download", "ListadoDeAfiliados.xlsx"); // Nombre del archivo
       document.body.appendChild(link);
       link.click();
       link.remove();
