19b49ab6c94bd2946a351fc263e1bd3ef477ed6a
Augusto <villevip10@gmail.com>
2024-10-14 20:50:59 -0300

    adv

--- src/components/Mailing.jsx
+++ src/components/Mailing.jsx
@@ -7,15 +7,18 @@ export const Mailing = () => {
         <h1 className="text-3xl text-gray-200">Mailing</h1>
       </div>
 
-        <div className="bg-secondary-100 p-24 rounded-lg">
+      <div className="bg-secondary-100 p-24 rounded-lg">
+        <div className="mb-3">
+        <h1>Emails</h1>
+        <input type="text" />
+        </div>
+
         <div>
-            
+        <button className="bg-green-color text-black font-bold py-3 px-2 rounded-md"> Send Emails </button>
         </div>
 
-
-
-        </div>
-
+      
+      </div>
     </div>
   );
 };
