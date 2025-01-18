f73592e833866f720c6964ff0bffa93ee40fb852
Augusto <villevip10@gmail.com>
2025-01-18 02:10:26 -0300

    adv

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import tailwindcss from 'tailwindcss';
-import autoprefixer from 'autoprefixer';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "tailwindcss";
+import autoprefixer from "autoprefixer";
 
 export default defineConfig({
   //base: "https://beche10.github.io/08-frontUL/", // Asegúrate de que la base coincida con el nombre del repositorio en GitHub
